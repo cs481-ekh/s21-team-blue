70a9b014119e6e5a2095d62c32c58f0d89b83697
Max Hanson <29157840+mmhanson@users.noreply.github.com>
2021-02-11 08:44:53 -0700

    Moved setuptools to apt-get from pip

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # controller 'building'
+sudo apt-get install python3-setuptools # so the next pip command can setup its packages. Note the CI container uses passwordless sudo
 pip3 install -r controller/requirements.txt
 pylint3 controller/main.py
 
