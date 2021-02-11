34ad8f5826a28c1a5519dec4b6e398a04a331d2e
Max Hanson <29157840+mmhanson@users.noreply.github.com>
2021-02-11 08:47:08 -0700

    Moved pylint install to apt from pip

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # controller 'building'
 sudo apt-get install python3-setuptools # so the next pip command can setup its packages. Note the CI container uses passwordless sudo
-pip3 install -r controller/requirements.txt
+sudo apt-get install pylint3
+#pip3 install -r controller/requirements.txt
 pylint3 controller/main.py
 
