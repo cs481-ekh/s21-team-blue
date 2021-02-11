4c8fad148ac58be0f0cb8938d83972b4c03dbeef
Max Hanson <29157840+mmhanson@users.noreply.github.com>
2021-02-11 08:49:10 -0700

    Moved pylint installation to apt instead of pip. Build script works now.

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # controller 'building'
-sudo apt-get install python3-setuptools # so the next pip command can setup its packages. Note the CI container uses passwordless sudo
-sudo apt-get install pylint3
-#pip3 install -r controller/requirements.txt
+sudo apt-get install pylint3 # note the CI container uses passwordless sudo
 pylint3 controller/main.py
 
