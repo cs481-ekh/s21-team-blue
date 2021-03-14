4e0073524a8dcd585f33edf9c4c46846dbb5afba
Max Hanson <29157840+mmhanson@users.noreply.github.com>
2021-03-14 15:47:07 -0600

    Added a full port scan and eternal blue vulnerability scan.

--- controller/basic_scans.py
+++ controller/basic_scans.py
@@ -32,16 +32,40 @@ def create_json_result(res_id, test_id, desc, date, val):
         "value" : val
     }
 
-# Basic nmap scan of common TCP ports on a given host
-def port_scan(test_id, host_ip, res_id):
-
+def eternal_blue_scan(test_id, host_ip, res_id):
     date = datetime.now()
     val = ""
     desc = ""
 
-    # all TCP ports
-    #port_range = "0-65535"
-    port_range = "21-443"
+    scanner = nmap.PortScanner()
+    scanner.scan(hosts=host_ip, ports="445", arguments="--script smb-vuln-ms17-010")
+
+    if not scanner.all_hosts():
+        desc = "No hosts detected"
+        val = "Info"
+        return create_json_result(res_id, test_id, desc, date, val)
+
+    host = scanner.all_hosts()[0]
+    if "hostscript" not in scanner[host].keys():
+        desc = "Host is secure."
+        val = "Success"
+    else:
+        desc = """Host is vulnerable!
+===Nmap Output===
+""" + scanner[host]["hostscript"][0]["output"]
+        val = "Failure"
+    return create_json_result(res_id, test_id, desc, date, val)
+
+def basic_port_scan(test_id, host_ip, res_id):
+    return port_scan(test_id, host_ip, res_id, "21-443")
+
+def full_port_scan(test_id, host_ip, res_id):
+    return port_scan(test_id, host_ip, res_id, "0-65535")
+
+def port_scan(test_id, host_ip, res_id, port_range):
+    date = datetime.now()
+    val = ""
+    desc = ""
     protocol = "tcp"
 
     scanner = nmap.PortScanner()
@@ -51,13 +75,9 @@ def port_scan(test_id, host_ip, res_id):
     if not scanner.all_hosts():
         desc = "No hosts detected"
         val = "Info"
-
-    # return if there are no tcp ports dectected
-    if not scanner.all_hosts():
-        desc = "No hosts found"
-        val = "Info"
         return create_json_result(res_id, test_id, desc, date, val)
 
+    # return if there are no tcp ports dectected
     host = scanner.all_hosts()[0]
     if "tcp" not in scanner[host].all_protocols():
         desc = "No open TCP ports found"
@@ -78,7 +98,9 @@ def port_scan(test_id, host_ip, res_id):
 # Mapping of Test IDs to functions
 def run_test(id, ip, idx):
     tests = {
-        1: port_scan(1, ip, idx),
+        1: basic_port_scan(1, ip, idx),
+        2: full_port_scan(2, ip, idx),
+        3: eternal_blue_scan(2, ip, idx),
     }
     return tests.get(id, "ERROR: Could not find a test with the given ID")
 
@@ -98,5 +120,6 @@ if __name__ == "__main__":
         exit(0)
     ip = args[0] # Grab the IP address
     test_ids = args[1].split(',') # Store the list of Test IDs as an array
+    #print("test_ids: " + str(test_ids))
     results = execute_tests(ip, test_ids) # Execute the tests and store the results
     print(json.dumps(results))
