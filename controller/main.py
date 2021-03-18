5c85c07c42de15c92752f82dab3cd9b76ab68947
Max Hanson <29157840+mmhanson@users.noreply.github.com>
2021-03-18 11:15:09 -0600

    Finished refactoring controller with new api. Put logic from basic_scans.py into main.py, created new test api, and moved old tests to the new api.

--- controller/main.py
+++ controller/main.py
@@ -1,18 +1,52 @@
 """
-Placeholder docstsring.
+Main file for the test controller.
+
+This file is responsible for enumerating the tests located in the 'scans' subdirectory and providing access.
+This file can be run manually, run it with '--help' for usage.
+This file can also be automated. Use the 'test_list()' function to get a list of tests to run, and use the
+execute_tests function to run the tests and receive the results.
+
+Written by Max Hanson, Spring 2021 for my BSU Senior Project.
 """
 
 
+from datetime import datetime
 import os
+import sys
+import json
+
+# import every '.py' in the 'scans' directory and record its name in 'tests' as a string
 tests = [x[:-3] for x in os.listdir("tests") if x.endswith(".py")]
-for test in tests: exec("from scans." + test + " import " + test)
+tests.remove("__init__") # __init__ is not a scan!
+for test in tests: exec("from tests." + test + " import " + test)
+
+# these globals are needed for the test_list function because of how exec() works
+name = ""
+desc = ""
+oses = ""
 
 
 def main():
     """
-    Placeholder docstring.
+    Manually run tests and view the results.
+
+    Run this file with the '--help' flag to see the usage guide.
     """
-    print("Hello world")
+
+    args = sys.argv[1:] # ignore the filename arg
+
+    if not args or args[0] == "--help":
+        print("Manual scanning:")
+        print("    --list: Print out a list of runnable tests")
+        print("    --ip ip_addr test_id0 test_id1 ...: Scan the host at ip_addr with tests id0, id1, ... and print the results")
+    elif args[0] == "--list":
+        print(tests)
+        [print(json.dumps(test)) for test in test_list()]
+    elif args[0] == "--ip":
+        ip = args[1]
+        test_ids = args[2:]
+        results = execute_tests(ip, test_ids) # Execute the tests and store the results
+        print(json.dumps(results))
 
 
 def test_list():
@@ -22,46 +56,48 @@ def test_list():
     See the README for details on the formatting.
     """
 
-    name = ""
-    desc = ""
-    oses = ""
-    tests = []
+    test_json_objs = []
     for test_id, test_name in enumerate(tests):
-        exec("name = " + test_name + ".name")
-        exec("desc = " + test_name + ".desc")
-        exec("oses = " + test_name + ".oses")
+        exec("name = "+test_name+".name", globals())
+        exec("desc = "+test_name+".desc", globals())
+        exec("oses = "+test_name+".oses", globals())
         test = {
             "test_id": str(test_id),
             "test_name": name,
             "test_description": desc,
             "operating_systems": oses,
         }
-        tests.append(test)
-    return tests
+        test_json_objs.append(test)
+    return test_json_objs
 
 
-# Creates the result object in JSON format
-def create_json_result(res_id, test_id, desc, date, val):
-    return {
-        "id": str(res_id),
-        "test_id": str(test_id),
-        "description": desc,
-        "date": date.strftime("%b %d %Y %H:%M:%S"),
-        "value" : val
-    }
+def execute_tests(ip, test_ids):
+    """
+    Execute a list of tests and return a list of json results.
 
-# Mapping of Test IDs to functions
-def run_test(id, ip, idx):
-    val = tests[id](ip)
+    See the README for details on the format of the json results.
 
-# Loop through all of the Test IDs, run the corresponding tests, and return the results
-def execute_tests(ip, tests):
+    @param ip The ip address of the host to scan
+    @param test_ids A list of ids of tests to run, must correspond to ids returned by test_list()
+    """
+
     results = []
-    idx = 0
-    for id in tests:
-        results.append(run_test(int(id), ip, idx))
-        idx += 1
+    for result_id, test_id in enumerate(test_ids):
+        test_name = tests[test_id]
+        test_val = ""
+        test_desc = ""
+        exec("test_val = " + test_name + ".scan(ip)")
+        exec("test_desc = " + test_name + ".desc")
+        test_result = {
+            "id": str(result_id),
+            "test_id": str(test_id),
+            "value": test_val,
+            "description": test_desc,
+            "date": datetime.now().strftime("%b %d %Y %H:%M:%S"),
+        }
+        results.append(test_result)
     return results
+
 
 if __name__ == "__main__":
     main()
