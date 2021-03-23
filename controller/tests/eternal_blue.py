e3fe7252488ce33ab81d5f7126362c693937cdd5
Max Hanson <29157840+mmhanson@users.noreply.github.com>
2021-03-23 10:43:23 -0600

    Added plugins for the eternal blue, nmap basic, and nmap full scans.

---
+++ controller/tests/eternal_blue.py
@@ -0,0 +1,37 @@
+"""
+Pirate scan for the eternal blue vulnerability (CVE MS17-010).
+
+Written by Max Hanson, Spring 2021 for my BSU Senior Project.
+"""
+
+import nmap
+
+
+class eternal_blue:
+    """
+    Scan object for the eternal blue vulnerability (CVE MS17-010).
+    """
+
+    name = "Eternal Blue"
+    desc = "Scans for the eternal blue vulnerability"
+    oses = "Windows"
+
+    def scan (self, host_ip):
+        """
+        Scan for the Eternal Blue vulnerability according to the Pirate Scan API.
+
+        @param host_ip The ip address of the host to scan for the vulnerability
+        """
+
+        scanner = nmap.PortScanner()
+        scanner.scan(hosts=host_ip, ports="445", arguments="--script smb-vuln-ms17-010")
+
+        if not scanner.all_hosts():
+            return "Failure. No hosts detected."
+
+        host = scanner.all_hosts()[0]
+        if "hostscript" not in scanner[host].keys():
+            return "Success. Host is not vulnerable to Eternal Blue (CVE MS17-010)."
+        else:
+            nmap_output = scanner[host]["hostscript"][0]["output"]
+            return "Failure. Host is vulnerable to Eternal Blue (CVE MS17-010).\n" + nmap_output
---
+++ controller/tests/full_port_scan.py
@@ -0,0 +1,45 @@
+"""
+Pirate scan for all TCP ports (0-65535).
+
+Written by Max Hanson, Spring 2021 for my BSU Senior Project.
+"""
+
+import nmap
+
+
+class full_port_scan:
+    """
+    Pirate scan object for all TCP ports (0-65535).
+    """
+
+    name = "Full Port Scan"
+    desc = "Scan TCP ports 0-65535 and interrogate those open ports for the services running on them."
+    oses = "Any"
+
+    def scan(self, host_ip):
+        """
+        Scan all TCP ports (0-65535) and determine if they are open.
+        If they are open, they are interrogated for the services running on those ports.
+
+        @param host_ip The ip address of the host to scan
+        """
+
+        scanner = nmap.PortScanner()
+        scanner.scan(host_ip, "0-65535")
+
+        if not scanner.all_hosts():
+            return "Failure. Host cannot be found."
+
+        host = scanner.all_hosts()[0]
+        if "tcp" not in scanner[host].all_protocols():
+            return "Success. No open TCP ports found on host."
+
+        open_ports = {}
+        scanned_ports = scanner[host]["tcp"].keys()
+        for port in scanned_ports:
+            port_name = scanner[host]["tcp"][port]["name"]
+            port_prod = scanner[host]["tcp"][port]["product"]
+            open_ports[port] = port_name + ", " + port_prod
+
+        return "Success. Open TCP ports: " + str(open_ports)
+
