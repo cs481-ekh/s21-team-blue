079da1576553e987e90ea181c9ef0b28571cbaf3
Max Hanson <29157840+mmhanson@users.noreply.github.com>
2021-03-30 11:06:39 -0600

    Finished bluekeep plugin.

--- controller/tests/bluekeep.py
+++ controller/tests/bluekeep.py
@@ -4,6 +4,8 @@ Pirate scan for the bluekeep vulnerability (CVE 2019-0708).
 Written by Max Hanson, Spring 2021 for my BSU Senior Project.
 """
 
+import subprocess
+
 
 class bluekeep:
     """
@@ -14,11 +16,16 @@ class bluekeep:
     desc = "Scans for the bluekeep vulnerability"
     oses = "Windows"
 
-    def scan (host_ip):
+    def scan (self, host_ip):
         """
         Scan for the Bluekeep vulnerability according to the Pirate Scan API.
 
         @param host_ip The ip address of the host to scan for the vulnerability
         """
 
-        return ("Failure", "Test not implemented.")
+        output = subprocess.run(["./rdpscan", "-v", host_ip], stdout=subprocess.PIPE)
+        output = output.stdout
+        if "VULNERABLE" in output:
+            return ("Failure.", output)
+        else:
+            return ("Success.", output)
