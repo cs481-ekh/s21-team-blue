3cb5a9a1c66e481bada2726dcdb2049e683efdcb
Max Hanson <29157840+mmhanson@users.noreply.github.com>
2021-03-30 14:52:24 -0600

    Bluekeep (#108)
    * Started test for bluekeep.
    
    * Finished bluekeep plugin.
    
    * Updated bluekeep script
    
    * Bluekeep is working with web interface.

---
+++ controller/tests/bluekeep.py
@@ -0,0 +1,34 @@
+"""
+Pirate scan for the bluekeep vulnerability (CVE 2019-0708).
+
+Written by Max Hanson, Spring 2021 for my BSU Senior Project.
+"""
+
+import subprocess
+import os
+
+
+class bluekeep:
+    """
+    Scan object for the bluekeep vulnerability (CVE 2019-0708).
+    """
+
+    name = "Bluekeep"
+    desc = "Scans for the bluekeep vulnerability"
+    oses = "Windows"
+
+    def scan (host_ip):
+        """
+        Scan for the Bluekeep vulnerability according to the Pirate Scan API.
+
+        @param host_ip The ip address of the host to scan for the vulnerability
+        """
+
+        scriptpath = os.path.dirname(os.path.realpath(__file__))
+        rdpscan_cmd = scriptpath + "/rdpscan"
+        output = subprocess.run([rdpscan_cmd, "-v", host_ip], stdout=subprocess.PIPE)
+        output = output.stdout.decode("UTF-8")
+        if "VULNERABLE" in output:
+            return ("Failure.", "Host is vulnerable, rdpscan output: " + output)
+        else:
+            return ("Success.", "Host is not vulnerable, rdpscan output: " + output)
