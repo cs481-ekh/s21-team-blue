2eb592e5ba8591d890483b2a0acb748001341bd6
Cole Gilmore <47162040+Colmine@users.noreply.github.com>
2021-04-06 06:40:44 -0600

    fully running auto-install + start (#106)

--- backend/serverscript.py
+++ backend/serverscript.py
@@ -7,18 +7,22 @@ subprocess.run(["sudo", "apt", "install", "dnsmasq", "-y"], stdout=subprocess.DE
 subprocess.run(["sudo", "systemctl", "restart", "dnsmasq"], stdout=subprocess.DEVNULL)
 subprocess.run(["sudo", "apt", "install", "dnsutils", "-y"], stdout=subprocess.DEVNULL)
 subprocess.run(["sudo", "apt-get", "install", "python3.6", "-y"], stdout=subprocess.DEVNULL)
+subprocess.run(["sudo", "apt-get", "install", "nmap", "-y"], stdout=subprocess.DEVNULL)
 
 nvm = subprocess.Popen(["wget", "-qO-", "https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh"], stdout=subprocess.PIPE)
 subprocess.run(["bash"], stdin=nvm.stdout, stdout=subprocess.DEVNULL)
-subprocess.run(["nvm", "install", "15.10.0"], stdout=subprocess.DEVNULL)
+proc = "nvm install 15.10.0"
+run = subprocess.Popen(["/bin/bash", "-i", "-c", proc])
+run.communicate()
+exit = run.wait()
 
-subprocess.run(["cd", "/home/pi/s21-team-blue/web-app"], stdout=subprocess.DEVNULL)
-subprocess.run(["sudo", "npm", "ci"], stdout=subprocess.DEVNULL)
-subprocess.run(["sudo", "npm", "run", "pro-build"], stdout=subprocess.DEVNULL)
+subprocess.run(["cd", "/home/pi/s21-team-blue/web-app"], shell=True, stdout=subprocess.DEVNULL)
+subprocess.run(["npm", "ci"], shell=True, stdout=subprocess.DEVNULL)
+subprocess.run(["npm", "run", "pro-build"], shell=True, stdout=subprocess.DEVNULL)
 
-subprocess.run(["cd", "/home/pi/s21-team-blue/backend"], stdout=subprocess.DEVNULL)
-subprocess.run(["sudo", "npm", "ci"], stdout=subprocess.DEVNULL)
-subprocess.run(["sudo", "npm", "start"], stdout=subprocess.DEVNULL)
+subprocess.run(["cd", "/home/pi/s21-team-blue/backend"], shell=True, stdout=subprocess.DEVNULL)
+subprocess.run(["npm", "ci"], shell=True, stdout=subprocess.DEVNULL)
+subprocess.run(["npm", "start"], shell=True, stdout=subprocess.DEVNULL)
 
 # sudo apt-get update -y
 # sudo apt-get upgrade -y
